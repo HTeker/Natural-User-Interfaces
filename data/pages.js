ddfbce25ae084c3d58f2c5ca75282df580acbeb1
DESKTOP-E71E6KE\Halil <h.teker.developer@gmail.com>
2018-12-05 21:33:10 +0100

    no message

---
+++ data/pages.js
@@ -0,0 +1,112 @@
+var pages = {
+    "goal": {
+        "title": "Hoofdvraag",
+        "content": [{
+            "type": "paragraph",
+            "value": "Hoofdvraag"
+        }]
+    },
+    "conclusion": {
+        "title": "Eindconclusie",
+        "content": [{
+            "type": "paragraph",
+            "value": "Eindconclusie"
+        }]
+    },
+    "discussion": {
+        "title": "Aanbevelingen/discussie",
+        "content": [{
+            "type": "paragraph",
+            "value": "Aanbevelingen/discussie"
+        }]
+    },
+    "sources": {
+        "title": "Bronnenlijst",
+        "content": [{
+            "type": "paragraph",
+            "value": "Bronnenlijst"
+        }]
+    },
+    "brain": {
+        "title": "Brain",
+        "heroImage": "",
+        "content": [{
+            "type": "paragraph",
+            "value": "Brain"
+        }]
+    },
+    "eyeTracking": {
+        "title": "Eye-tracking",
+        "heroImage": "",
+        "content": [{
+            "type": "paragraph",
+            "value": "Eye-tracking"
+        }]
+    },
+    "voiceControl": {
+        "title": "Voice control",
+        "heroImage": "",
+        "content": [{
+            "type": "paragraph",
+            "value": "Voice control"
+        }]
+    },
+    "gestures": {
+        "title": "Gestures",
+        "heroImage": "",
+        "content": [{
+            "type": "paragraph",
+            "value": "Gestures"
+        }]
+    },
+    "touchscreen": {
+        "title": "Touchscreen",
+        "heroImage": "https://www.engineersgarage.com/sites/default/files/wysiwyg_imageupload/1/Pictorial-Representation-of-Touch-Screen-Technology.jpg",
+        "content": [{
+            "type": "accordion",
+            "value": "Deelvragen",
+            "children": [{
+                "type": "paragraph",
+                "value": "Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen."
+            }]
+        }, {
+            "type": "accordion",
+            "value": "Wat is het?",
+            "children": [{
+                "type": "paragraph",
+                "value": "Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen."
+            }]
+        }, {
+            "type": "accordion",
+            "value": "Toepassingen",
+            "children": [{
+                "type": "paragraph",
+                "value": "Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen."
+            }]
+        }, {
+            "type": "accordion",
+            "value": "Positieve punten",
+            "children": [{
+                "type": "paragraph",
+                "value": "Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen."
+            }]
+        }, {
+            "type": "accordion",
+            "value": "Hoe geschikt is het?",
+            "children": [{
+                "type": "paragraph",
+                "value": "Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen."
+            }]
+        }, {
+            "type": "accordion",
+            "value": "Wat zijn de voordelen en de nadelen?",
+            "children": [{
+                "type": "paragraph",
+                "value": "Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen."
+            }]
+        }, {
+            "type": "video",
+            "value": "https://www.youtube.com/embed/wKuqNuzM1oM"
+        }]
+    }
+}
--- script/PageRenderer.js
+++ script/PageRenderer.js
@@ -1,22 +1,69 @@
 PageRenderer = {
 
-    render(nui) {
+    render(page) {
 
         let html = '';
         
-        html += this.renderTitle(nui.title);
-        html += this.renderHeroImage(nui.heroImage);
+        html += this.renderTitle(page.title);
+        typeof(page.heroImage) != 'undefined' && (html += this.renderHeroImage(page.heroImage));
+        html += this.renderContent(page.content);
 
         return html
 
     },
 
     renderTitle(title) {
+
         return `<h1>${title}</h1>`;
+
     },
 
     renderHeroImage(imgSrc) {
+
         return `<img src="${imgSrc}" />`;
+
+    },
+
+    renderContent(content) {
+
+        let html = '';
+
+        content.forEach(item => {
+
+            switch(item.type) {
+
+                case 'accordion':
+                    html += `<h2>${item.value}</h2>`;
+                    break;
+
+                case 'paragraph':
+                    html += `<p>${item.value}</p>`
+                    break;
+
+                case 'video':
+                    html += `<iframe width="560" height="315" src="${item.value}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+                    break;
+
+                case 'html':
+                    html += item.value;
+                    break;                    
+    
+                default:
+                    html += '';
+                    break;
+    
+            }
+
+            if(Array.isArray(item.children)) {
+
+                html += this.renderContent(item.children);
+
+            }
+
+        });
+
+        return html;
+
     }
 
 };
