6037878427eb2ba2c3670db345cc71936dfba041
Halil Teker <h.teker.developer@gmail.com>
2018-12-06 13:13:34 +0100

    no message

--- script/PageRenderer.js
+++ script/PageRenderer.js
@@ -20,7 +20,7 @@ PageRenderer = {
 
     renderHeroImage(imgSrc) {
 
-        return `<img src="${imgSrc}" />`;
+        return `<div class="hero-img-container"><img src="${imgSrc}" /></div>`;
 
     },
 
@@ -33,31 +33,44 @@ PageRenderer = {
             switch(item.type) {
 
                 case 'accordion':
-                    html += `<h2>${item.value}</h2>`;
+
+                    html +=
+                    `<section class="accordion">
+                        <input type="radio" name="accordion" id="${item.value.toLowerCase()}">
+                        <h2 class="handle">
+                            <label for="${item.value.toLowerCase()}">${item.value}</label>
+                        </h2>
+                        <div class="content">
+                            ${this.renderContent(item.children)}
+                        </div>
+                    </section>`;
+
                     break;
 
                 case 'paragraph':
+
                     html += `<p>${item.value}</p>`
+
                     break;
 
                 case 'video':
+
                     html += `<iframe width="560" height="315" src="${item.value}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
                     break;
 
                 case 'html':
+
                     html += item.value;
+
                     break;                    
     
                 default:
+
                     html += '';
+
                     break;
     
-            }
-
-            if(Array.isArray(item.children)) {
-
-                html += this.renderContent(item.children);
-
             }
 
         });
